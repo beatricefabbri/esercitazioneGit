35049879a8969a3752835822495e1a9a6437a1f7
Beatrice Fabbri <BEATRICE.FABBRI@local.ittcesena.it>
2022-11-17 08:34:42 +0100

    branch libro

---
+++ libreria/libro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria
+{
+    internal class libro
+    {
+        //l’autore, il titolo, l’anno di pubblicazione, l’editore e il numero di pagine.
+        internal string autore { get; set; }
+        internal string titolo { get; set; }
+        internal int anno { get; set; }
+        internal string editore { get; set; }
+        internal int numeroDiPagine { get; set; }
+
+        internal libro(string autore, string titolo, int anno, string editore, int numeroDiPagine)
+        {
+            this.autore = autore;
+            this.titolo = titolo;
+            this.anno = anno;
+            this.editore = editore;
+            this.numeroDiPagine = numeroDiPagine;
+        }
+
+        static int readingTime(int numeroDiPagine)
+        {
+            int tempo = 0;
+            if(numeroDiPagine < 100)
+            {
+                tempo = 1;
+
+            }else if(numeroDiPagine > 100 && numeroDiPagine < 200)
+            {
+                tempo = 2;
+            }
+            else
+            {
+                tempo = 3;
+            }
+
+            return tempo;
+        }
+
+        static string toString(string autore, string titolo, int anno, string editore, int numeroDiPagine)
+        {
+            string stringaDiDati= "";
+            stringaDiDati += autore;
+            stringaDiDati += titolo;
+            stringaDiDati += anno;
+            stringaDiDati += editore;
+            stringaDiDati += numeroDiPagine;
+
+            return stringaDiDati;
+        }
+    }
+}
